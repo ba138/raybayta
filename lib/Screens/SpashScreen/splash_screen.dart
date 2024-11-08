1ecc4e2c5f68846488e2cbc268ebcaf186605d1b
Basit ali <basitalyshah51214@gmail.com>
2024-11-08 08:41:33 -0800

    changes to new icons in splash screen

--- lib/Screens/SpashScreen/splash_screen.dart
+++ lib/Screens/SpashScreen/splash_screen.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:raybayta/Configs/colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Logo",
+                style: GoogleFonts.getFont(
+                  "Poppins",
+                  textStyle: TextStyle(
+                    fontSize: 30,
+                    color: AppColors.darkOnBackgroundColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                "Raybayta",
+                style: GoogleFonts.getFont(
+                  "Poppins",
+                  textStyle: TextStyle(
+                    fontSize: 24,
+                    color: AppColors.darkOnBackgroundColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(
+                        "https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-173524.jpg"),
+                  ),
+                  SizedBox(width: 12),
+                  Icon(Icons.arrow_forward),
+                  SizedBox(width: 12),
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(
+                        "https://static.vecteezy.com/system/resources/thumbnails/004/899/833/small_2x/beautiful-girl-with-blue-hair-avatar-of-woman-for-social-network-vector.jpg"),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
