5118c09ce78dccd8d550076cb3bb1e827038f5f5
Basit ali <basitalyshah51214@gmail.com>
2024-11-10 02:36:11 -0800

    setting up app bar in home screen

--- lib/Screens/HomeScreen/home_screen.dart
+++ lib/Screens/HomeScreen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:raybayta/Configs/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,8 +9,29 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Raybayta"),
-        backgroundColor: AppColors.darkOnContainerColor,
+        title: Text(
+          "Raybayta",
+          style: GoogleFonts.getFont(
+            "Poppins",
+            textStyle: TextStyle(
+              fontSize: 18,
+              color: AppColors.darkOnBackgroundColor,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        backgroundColor: AppColors.darkContainerColor,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
+        leading: const Center(child: Text("logo")),
       ),
     );
   }
