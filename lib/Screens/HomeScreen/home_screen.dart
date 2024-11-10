219e6757b248dea9fedd9b7fed61c9a574e88e36
Basit ali <basitalyshah51214@gmail.com>
2024-11-10 02:27:32 -0800

    setting ui for home

--- lib/Screens/HomeScreen/home_screen.dart
+++ lib/Screens/HomeScreen/home_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:raybayta/Configs/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Raybayta"),
+        backgroundColor: AppColors.darkOnContainerColor,
+      ),
+    );
   }
 }
