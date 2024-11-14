3f0320a091566c590f0ebcf61e2442bb8a146b7b
Basit ali <basitalyshah51214@gmail.com>
2024-11-14 03:19:15 -0800

    calling login and signup function in the ui

--- lib/Controllers/auth_controller.dart
+++ lib/Controllers/auth_controller.dart
@@ -4,30 +4,51 @@ import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   final auth = FirebaseAuth.instance;
+  RxBool isLoading = false.obs;
   Future<void> login(String email, String password) async {
+    isLoading.value = true;
     try {
       await auth.signInWithEmailAndPassword(email: email, password: password);
+      isLoading.value = false;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
+        isLoading.value = false;
+
         Get.snackbar("Error", "User not Found");
       } else if (e.code == 'wrong-password') {
+        isLoading.value = false;
+
         Get.snackbar("Error", "Wrong password");
       }
     } catch (e) {
+      isLoading.value = false;
+
       debugPrint(e.toString());
     }
   }
 
   Future<void> createUser(String email, String password) async {
+    isLoading.value = true;
+
     try {
-      auth.createUserWithEmailAndPassword(email: email, password: password);
+      auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      isLoading.value = false;
     } on FirebaseAuthException catch (e) {
       if (e.code == "weak passowrd") {
+        isLoading.value = false;
+
         Get.snackbar("Warning", "Weak Password");
       } else if (e.code == "email-already-in-use") {
+        isLoading.value = false;
+
         Get.snackbar("Error", "Email is already register");
       }
     } catch (e) {
+      isLoading.value = false;
+
       debugPrint(
         e.toString(),
       );
